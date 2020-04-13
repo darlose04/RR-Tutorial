cbef4290b5dc0018ca5566fa4db5141237cd2ff1
Zach Smith <zsmith126028@gmail.com>
2020-04-13 15:45:32 -0500

    change COLUMNS to columns

--- src/components/StoriesHeader.js
+++ src/components/StoriesHeader.js
@@ -3,9 +3,9 @@ import React from "react";
 const StoriesHeader = ({ columns }) => {
   return (
     <div className="stories-header">
-      {Object.keys(COLUMNS).map((key) => (
-        <span key={key} style={{ width: COLUMNS[key].width }}>
-          {COLUMNS[key].label}
+      {Object.keys(columns).map((key) => (
+        <span key={key} style={{ width: columns[key].width }}>
+          {columns[key].label}
         </span>
       ))}
     </div>
