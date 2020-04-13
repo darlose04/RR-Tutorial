a5814b8cdea6e80aa6552409f59a8eeef09525bf
Zach Smith <zsmith126028@gmail.com>
2020-04-13 15:31:37 -0500

    add column info for better organization

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -3,6 +3,28 @@ import "./Stories.css";
 
 import Story from "./Story";
 
+const COLUMNS = {
+  title: {
+    label: "Title",
+    width: "40%",
+  },
+  author: {
+    label: "Author",
+    width: "30%",
+  },
+  comments: {
+    label: "Comments",
+    width: "10%",
+  },
+  points: {
+    label: "Points",
+    width: "10%",
+  },
+  archive: {
+    width: "10%",
+  },
+};
+
 const Stories = ({ stories }) => {
   return (
     <div>
