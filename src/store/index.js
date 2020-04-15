b1b2829aee0e7d1a8c088e6d92f4bca43f84792f
Zach Smith <zsmith126028@gmail.com>
2020-04-15 14:24:30 -0500

    import saga middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,21 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import { createLogger } from "redux-logger";
+import createSagaMiddleware from 'redux-saga';
 import rootReducer from "../reducers";
+import rootSaga from '../sagas';
 
 const logger = createLogger();
+const saga = createSagaMiddleware();
 
 const store = createStore(
   rootReducer,
   undefined,
   compose(
-    applyMiddleware(logger),
+    applyMiddleware(saga, logger),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
+
+saga.run(rootSaga);
 
 export default store;
