2181db14bfab84b895c11498319f65132f5f3fbc
Zach Smith <zsmith126028@gmail.com>
2020-04-15 11:55:30 -0500

    re render whole component tree each time the store updates

--- src/index.js
+++ src/index.js
@@ -7,15 +7,20 @@ import "./index.css";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App
-      stories={getReadableStories(store.getState())}
-      onArchive={(id) => store.dispatch({ type: STORY_ARCHIVE, id })}
-    />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+function render() {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App
+        stories={getReadableStories(store.getState())}
+        onArchive={(id) => store.dispatch({ type: STORY_ARCHIVE, id })}
+      />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+}
+
+store.subscribe(render);
+render();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
