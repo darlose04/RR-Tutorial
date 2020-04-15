890b325e10b652cd9f37ec600863752ffeccb380
Zach Smith <zsmith126028@gmail.com>
2020-04-15 12:07:16 -0500

    add redux logger, use compose to combine store enhancers

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,16 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import { createLogger } from "redux-logger";
 import rootReducer from "../reducers";
+
+const logger = createLogger();
 
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  undefined,
+  compose(
+    applyMiddleware(logger),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
 
 export default store;
